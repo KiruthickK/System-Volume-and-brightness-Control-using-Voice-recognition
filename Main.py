1ef76d228db9407317a0c4e66ebad514c9284f8c
KiruthickK <kiruthick101@gmail.com>
2023-03-21 19:18:07 +0530

    Some minor changes

--- Main.py
+++ Main.py
@@ -1,11 +1,8 @@
 import VoiceRecognition as VR
 import VoiceOutput as V_OP
-import os
 import VolumeControl as VC
 import BrightnessControl as BC
 import SpeechCommandDataset as DS
-#forincrease = ["increase the volume","up the volume"]
-# volumeIncreaseCommands = ["Increase volume","volume Increase"]
 
 flag = False
 while(True):
@@ -44,18 +41,11 @@ while(True):
         
         #for brightness
         elif(text in DS.IncreaseBrightness):
-            # VC.VolumeIncrease()
-            # print("here")
-            # V_OP.speech("Increased")
-            # print("Brightness increased")
-            # print("Increased the volume")
             BC.BrightnessIncrease()
             V_OP.speech("Brightness Increased")
         elif(text in DS.DecreaseBrightness):
-            # VC.VolumeDecrease()
             BC.BrightnessDecrease()
-            V_OP.speech("Brightness decreased")
-            # V_OP.speech("Decreased") 
+            V_OP.speech("Brightness decreased") 
         elif(text in DS.FullBrightness):
             BC.BrightnessFull()
             V_OP.speech("Brightness set to maximum")
@@ -70,13 +60,10 @@ while(True):
         elif(text in DS.Stop):
             flag = False
             V_OP.speech("Service stopping, until you start again")
-            # exit()
         elif(text in DS.stopCode):
             V_OP.speech("Thank you for using our service. use again!")
             exit()
         else:
             V_OP.speech("Check your voice command. Did you just said "+text+"?")
+            print("Check your voice command. Did you just said "+text+"?")
     
-        
-    
-
