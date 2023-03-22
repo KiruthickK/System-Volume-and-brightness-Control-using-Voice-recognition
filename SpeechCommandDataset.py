68ded953c23ae0ed9cf79bbb5bc1e827da7cd7b5
KiruthickK <kiruthick101@gmail.com>
2023-03-22 09:29:12 +0530

    Some changes in dataset

--- SpeechCommandDataset.py
+++ SpeechCommandDataset.py
@@ -2,15 +2,19 @@
 IncreaseVolume = ["increase the volume","turn up the volume","increase volume","increase sound","i need more sound"]
 VolumeMute = ["mute the volume","mute","mute volume","mute sound","no sound"]
 VolumeUnMute = ["unmute the volume","unmute","unmute volume","full volume","set volume to full","set volume to maximum"]
-DecreaseVolume = ["decrease the volume","turn down the volume","decrease volume","decrease sound"]
+DecreaseVolume = ["decrease the volume","turn down the volume","decrease volume","decrease sound","reduce volume",
+                  "reduce system volume","reduce the system volume","reduce the volume","reduce sound",
+                  "reduce the system sound","reduce system sound"]
 IncreaseBrightness = ["increase the brightness","turn up the brightness","increase brightness"]
-DecreaseBrightness = ["decrease the brightness","turn down the brightness","decrease brightness","low brightness"]
+DecreaseBrightness = ["decrease the brightness","turn down the brightness","decrease brightness","low brightness",
+                      "lower the brightness","reduce the brightness"]
 FullBrightness = ["set the maximum brightness","turn up the brightness to maximum","maximum brightness","full brightness"]
-FullLowBrightness = ["set the minimum brightness","turn down the brightness to minimum","minimum brightness","full low brightness"]
+FullLowBrightness = ["set the minimum brightness","turn down the brightness to minimum","minimum brightness",
+                     "full low brightness"]
 HalfVolume = ["half the volume","set volume to half","half volume","set half volume"]
 BrightnessHalf = ["half the brightness","set brightness to half","half brightness","set half brightness"]
-Stop = ["stop","top","close the service","end the service"]
-start = ["start","start services"]
+Stop = ["stop","top","close the service","end the service","terminate the program"]
+start = ["start","start services","start service"]
 stopCode = ["exit code","stop the program","exit","exit program"]
 Yes = ["yes","yes stop the service","yes end the service","yes stop the program","yeah i want","yeah"]
 No = ["no","i said by mistake","no i dont want","no i said by mistake","no i said by mistake sorry","nope"]
