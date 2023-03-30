a5459cf99023932fbb7ee37d9f186482fb62591f
NastY472 <arinbala2003@gamil.com>
2023-03-30 11:51:31 +0530

    Added some colors!

--- UsabilityHelpers.py
+++ UsabilityHelpers.py
@@ -1,15 +1,20 @@
 import shutil
+import colorama 
+from colorama import Fore, Back, Style
+colorama.init()
 def SingleLinePrinter():
     width = shutil.get_terminal_size().columns
-    print("-"*width)
+    print(Fore.GREEN +("-"*width)+ Style.RESET_ALL)
 def DesignPrinter():
     width = shutil.get_terminal_size().columns
     width = width/2
     width = int(width)-1
-    print(("=#" * width)+"=")
+    print(((Fore.WHITE +"="+Fore.RED +"#") * width+"=")+ Style.RESET_ALL)
+    # print(("=#" * width)+"=")
 def PrintRightSide(text):
     # Get the current console width
     width = shutil.get_terminal_size().columns
     padding = width - len(text)
     print(" " * padding + text)
-# DesignPrinter()
+DesignPrinter()
+# SingleLinePrinter()
